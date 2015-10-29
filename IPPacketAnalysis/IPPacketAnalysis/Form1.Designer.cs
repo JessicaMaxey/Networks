d7d4c42a8d34995e044a924b3581844fa64d01d6
JessicaMaxey <zilainalynn14@gmail.com>
2015-10-28 21:41:59 -0700

    Created file, set up socket binding

---
+++ IPPacketAnalysis/IPPacketAnalysis/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace IPPacketAnalysis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start_btn = new System.Windows.Forms.Button();
+            this.ip_txtbx = new System.Windows.Forms.TextBox();
+            this.transport_txtbx = new System.Windows.Forms.TextBox();
+            this.app_layer_txtbx = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(12, 546);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(75, 23);
+            this.start_btn.TabIndex = 0;
+            this.start_btn.Text = "Start";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // ip_txtbx
+            // 
+            this.ip_txtbx.Location = new System.Drawing.Point(12, 12);
+            this.ip_txtbx.Multiline = true;
+            this.ip_txtbx.Name = "ip_txtbx";
+            this.ip_txtbx.Size = new System.Drawing.Size(570, 172);
+            this.ip_txtbx.TabIndex = 1;
+            // 
+            // transport_txtbx
+            // 
+            this.transport_txtbx.Location = new System.Drawing.Point(12, 190);
+            this.transport_txtbx.Multiline = true;
+            this.transport_txtbx.Name = "transport_txtbx";
+            this.transport_txtbx.Size = new System.Drawing.Size(570, 172);
+            this.transport_txtbx.TabIndex = 2;
+            // 
+            // app_layer_txtbx
+            // 
+            this.app_layer_txtbx.Location = new System.Drawing.Point(12, 368);
+            this.app_layer_txtbx.Multiline = true;
+            this.app_layer_txtbx.Name = "app_layer_txtbx";
+            this.app_layer_txtbx.Size = new System.Drawing.Size(570, 172);
+            this.app_layer_txtbx.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 576);
+            this.Controls.Add(this.app_layer_txtbx);
+            this.Controls.Add(this.transport_txtbx);
+            this.Controls.Add(this.ip_txtbx);
+            this.Controls.Add(this.start_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.TextBox ip_txtbx;
+        private System.Windows.Forms.TextBox transport_txtbx;
+        private System.Windows.Forms.TextBox app_layer_txtbx;
+    }
+}
+
---
+++ IPPacketAnalysis/IPPacketAnalysis/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+
+namespace IPPacketAnalysis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            //IPHostEntry ip_host_info = Dns.GetHostEntry("");
+            IPHostEntry ip_host_info = Dns.Resolve(Dns.GetHostName());
+            IPAddress ip_address = ip_host_info.AddressList[0];
+            IPEndPoint end_point = new IPEndPoint(ip_address, 0);
+
+            Socket com_sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //com_sock.Connect(end_point);
+            com_sock.Bind(end_point);
+
+            NetworkStream net_stream = new NetworkStream(com_sock);
+            
+            StringBuilder derp = new StringBuilder();
+            while (true)
+            {
+                 derp.Append(net_stream.ReadByte());
+            }
+        }
+    }
+}
