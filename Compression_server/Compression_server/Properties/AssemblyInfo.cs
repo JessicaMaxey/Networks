b4439bcfeb38e0a13cba0cd333f391fb6e96420d
JessicaMaxey <zilainalynn14@gmail.com>
2015-11-16 11:51:01 -0800

    created new server since old one was corupted

--- Compression_server/Compression_server/Properties/AssemblyInfo.cs
+++ Compression_server/Compression_server/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Compression_Server_v2")]
+[assembly: AssemblyTitle("Compression_server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Compression_Server_v2")]
+[assembly: AssemblyProduct("Compression_server")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("875ac4e0-73d6-4bf1-ad8f-dc4c19ced74f")]
+[assembly: Guid("620ba628-34dd-423c-8982-39de2b6b0122")]
 
 // Version information for an assembly consists of the following four values:
 //
